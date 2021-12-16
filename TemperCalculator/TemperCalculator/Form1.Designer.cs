0f4330a4312bcdd395a4a61508f428f7efa3933f
miyazagiOO <96216663+miyazagiOO@users.noreply.github.com>
2021-12-16 12:15:55 +0700

    thatsanee

---
+++ TemperCalculator/TemperCalculator/Form1.Designer.cs
@@ -0,0 +1,132 @@
+﻿namespace TemperCalculator
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.buttonConvert = new System.Windows.Forms.Button();
+            this.textBoxC = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.textBoxF = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.buttonConvert2 = new System.Windows.Forms.Button();
+            this.buttonclear = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // buttonConvert
+            // 
+            this.buttonConvert.Location = new System.Drawing.Point(138, 49);
+            this.buttonConvert.Name = "buttonConvert";
+            this.buttonConvert.Size = new System.Drawing.Size(75, 23);
+            this.buttonConvert.TabIndex = 0;
+            this.buttonConvert.Text = "convert";
+            this.buttonConvert.UseVisualStyleBackColor = true;
+            this.buttonConvert.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // textBoxC
+            // 
+            this.textBoxC.Location = new System.Drawing.Point(12, 49);
+            this.textBoxC.Name = "textBoxC";
+            this.textBoxC.Size = new System.Drawing.Size(100, 20);
+            this.textBoxC.TabIndex = 1;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(118, 52);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(14, 13);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "C";
+            // 
+            // textBoxF
+            // 
+            this.textBoxF.Location = new System.Drawing.Point(12, 75);
+            this.textBoxF.Name = "textBoxF";
+            this.textBoxF.Size = new System.Drawing.Size(100, 20);
+            this.textBoxF.TabIndex = 3;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(118, 78);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(13, 13);
+            this.label2.TabIndex = 4;
+            this.label2.Text = "F";
+            // 
+            // buttonConvert2
+            // 
+            this.buttonConvert2.Location = new System.Drawing.Point(137, 73);
+            this.buttonConvert2.Name = "buttonConvert2";
+            this.buttonConvert2.Size = new System.Drawing.Size(75, 23);
+            this.buttonConvert2.TabIndex = 5;
+            this.buttonConvert2.Text = "convert";
+            this.buttonConvert2.UseVisualStyleBackColor = true;
+            this.buttonConvert2.Click += new System.EventHandler(this.buttonConvert2_Click);
+            // 
+            // buttonclear
+            // 
+            this.buttonclear.Location = new System.Drawing.Point(219, 53);
+            this.buttonclear.Name = "buttonclear";
+            this.buttonclear.Size = new System.Drawing.Size(75, 42);
+            this.buttonclear.TabIndex = 6;
+            this.buttonclear.Text = "clear";
+            this.buttonclear.UseVisualStyleBackColor = true;
+            this.buttonclear.Click += new System.EventHandler(this.buttonclear_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(304, 136);
+            this.Controls.Add(this.buttonclear);
+            this.Controls.Add(this.buttonConvert2);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.textBoxF);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.textBoxC);
+            this.Controls.Add(this.buttonConvert);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button buttonConvert;
+        private System.Windows.Forms.TextBox textBoxC;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox textBoxF;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Button buttonConvert2;
+        private System.Windows.Forms.Button buttonclear;
+    }
+}
+
---
+++ TemperCalculator/TemperCalculator/Form1.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace TemperCalculator
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            // input
+            // นี่คือคอมเม้น
+            //textBoxC.Text = "Hello";
+           //get input foem textbox
+            string Input = textBoxC.Text;
+
+            //convert to f
+            //f = c x 9/5 + 32;
+            double c = Convert.ToDouble(Input);
+            double f = c * 9 / 5 + 32;
+       
+            //show input to textbox
+            textBoxF.Text = f.ToString();
+        
+
+        }
+
+        private void buttonConvert2_Click(object sender, EventArgs e)
+        {
+
+            //get input form textbox
+            string input = textBoxF.Text;
+            //c = f 5/9 x (F-32)
+            double f = Convert.ToDouble(input);
+            double c = (f - 32) * 5 / 9;
+
+            textBoxC.Text = c.ToString();
+        }
+
+        private void buttonclear_Click(object sender, EventArgs e)
+        {
+            textBoxC.Text = " ";
+            textBoxF.Text = " ";
+        }
+    }
+}
